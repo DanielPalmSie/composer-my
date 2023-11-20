e36ddcda42b3eee1ab90aa0a09263ea2ee14a5bf
daniel <palm6991@gmail.com>
2023-11-20 02:09:13 +0100

    test

--- src/NumberGenerator.php
+++ src/NumberGenerator.php
@@ -6,7 +6,7 @@ class NumberGenerator
 {
     public function sumNumbersRecursively($number, $sum = 0) {
         // Добавляем текущее число к сумме
-        $sum += $number;
+        $sum += $number;//
 
         // Если число меньше 10, продолжаем рекурсию с увеличенным значением
         if ($number < 10) {
